41a5e6b08f433d30fc1852072f38f8ec509ffb7b
Abby Benjamin <ablizben@gmail.com>
2020-10-19 19:47:31 -0400

    Updating server file to connect to routes and mongoose

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
+require("dotenv").config();
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
+const routes = require("./routes");
+
+//Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
+
+//Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(routes);
 
-// Define API routes here
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//Connecting to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
+
+//Starting the server
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
